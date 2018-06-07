71062ad8a13b43ae4a9e2c66429e3c6a2f2cda22
wilaman21 <williantecnico1998@gmail.com>
2018-06-07 20:49:00 -0300

    ajustes

--- src/main/java/br/edu/unichristus/mvcprojetos/controllers/IndexController.java
+++ src/main/java/br/edu/unichristus/mvcprojetos/controllers/IndexController.java
@@ -1,11 +1,21 @@
 package br.edu.unichristus.mvcprojetos.controllers;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.unichristus.mvcprojetos.dados.entidades.Projeto;
 
 @Controller
 public class IndexController {
-@RequestMapping("/")
-public String index(){
+
+	@RequestMapping("/")
+	public String index(Model modelo) {
+		Projeto p1 = new Projeto();
+		p1.setNome("Projeto NASA Espacial");
+		p1.setDescricao("Projeto para Construção de Nave");
+		p1.setPatrocinador("NASA");
+		modelo.addAttribute("projeto1", p1);
 		return "index";
 	}
 }
--- src/main/java/br/edu/unichristus/mvcprojetos/controllers/ProjetosController.java
+++ src/main/java/br/edu/unichristus/mvcprojetos/controllers/ProjetosController.java
@@ -1,13 +1,56 @@
 package br.edu.unichristus.mvcprojetos.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.edu.unichristus.mvcprojetos.dados.entidades.Projeto;
+import br.edu.unichristus.mvcprojetos.dados.services.ProjetosService;
 
 @Controller
 @RequestMapping("/projeto")
 public class ProjetosController {
-	@RequestMapping("/adicionar")
+	
+	@Autowired
+	private ProjetosService servico;
+
+	@RequestMapping(value="/adicionar", method=RequestMethod.GET)
+	// Exibe o formulário para adicionar um projeto
 	public String adicionarProjeto() {
+		System.out.println("Invocando o formulário via GET");
 		return "adicionarProjeto";
 	}
+	
+	@RequestMapping(value="/adicionar", method=RequestMethod.POST)
+	// Salvar um projeto
+	public String salvarProjeto() {
+		System.out.println("Invocando o formulário via POST");
+		return "adicionarProjeto";
+	}
+	
+	@RequestMapping(value= "/adicionar", method=RequestMethod.POST, params={"tipo=multi"})
+	public String salvarProjetoMultiplosAnos(){
+		System.out.println("invocando salvar projeto múltiplos anos");
+		return "adicionarProjeto";
+	}
+	
+	@RequestMapping(value= "/adicionar", method=RequestMethod.POST, params={"tipo=multi", "especial"})
+	public String salvarProjetoMultiplosAnosEspecial(){
+		System.out.println("invocando salvar projeto múltiplos anos e especial");
+		return "adicionarProjeto";
+	}
+	
+	@RequestMapping(value = "/buscar")
+	public String buscarTodos(Model modelo) {
+		List<Projeto> projetos = this.servico.buscarTodos();
+		modelo.addAttribute("projetos", projetos);
+		return "projetos";
+	}
+
+
+
 }
